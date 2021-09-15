c0b861525955753e2a34ccc627bc55b6d1a03db9
dpopov14 <d.popov@student.utwente.nl>
2021-09-15 22:13:17 +0300

    DONE DONE DONE

--- app/src/main/java/com/example/indoorpositioning/MapActivity.java
+++ app/src/main/java/com/example/indoorpositioning/MapActivity.java
@@ -6,15 +6,27 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity  extends AppCompatActivity {
-
+public class MapActivity  extends AppCompatActivity implements OnMapReadyCallback {
+    private Double lat;
+    private Double lon;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         Button button;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_activity);
+
+
+//        Here we initialise the map
+        // Get the SupportMapFragment and request notification when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
 
         button = findViewById(R.id.back_button);
@@ -27,11 +39,22 @@ public class MapActivity  extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             double[] value = extras.getDoubleArray("latlon");
+            lat = value[0];
+            lon = value[1];
             System.out.println("Value is: " + value[0]);
             //The key argument here must match that used in the other activity
-
+//            this.onMapReady(mapFragment.getMapAsync());
         }
 
 
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng sydney = new LatLng(lat, lon);
+        googleMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
     }
 }
